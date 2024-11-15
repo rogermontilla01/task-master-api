1799aa1653d505a1031bc9ea2f1311d8ad25d6b6
Roger Montilla <rmontilla@wizards.us>
2024-11-15 09:14:46 -0500

    feat: add readme and postman collection

--- internal/employee/application/dtos/employee_dto.go
+++ internal/employee/application/dtos/employee_dto.go
@@ -10,7 +10,7 @@ type EmployeeDto struct {
 	ID             string    `json:"id"`
 	Name           string    `json:"name"`
 	Skills         []string  `json:"skills"`
-	AvailableHours string    `json:"hours"`
+	AvailableHours string    `json:"availableHours"`
 	CreatedAt      time.Time `json:"createdAt,omitempty"`
 	UpdatedAt      time.Time `json:"updatedAt,omitempty"`
 	DeletedAt      time.Time `json:"deletedAt,omitempty"`
